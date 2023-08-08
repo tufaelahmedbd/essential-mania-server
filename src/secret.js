def3e239ad6e1ed0bc2da286b2ab03a6cf65189b
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-08-09 00:28:51 +0600

    bug fixed

--- src/secret.js
+++ src/secret.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const serverPort = process.env.SERVER_PORT || 3002;
-const mongodbUri =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/essential-mania";
+const mongodbUri = process.env.MONGODB_URI;
 
 module.exports = { serverPort, mongodbUri };
