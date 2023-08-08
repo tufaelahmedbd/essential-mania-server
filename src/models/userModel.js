e998b12ebc7f66f38b95b1dbd2801bfaa6e3fd26
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-08-09 04:38:00 +0600

    model added

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,57 @@
-const users = [
-  { id: 1, name: "Tufael" },
-  { id: 2, name: "Rumel" },
-];
-module.exports = users;
+const { Schema, model } = require("mongoose");
+const { defaultImagePath } = require("../secret");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User name is required"],
+      trim: true,
+      minlength: [3, "The length of user name can be minimum 3 characters"],
+      maxlength: [31, "The length of user name can be maximum 31 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "User email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "User password is required"],
+      minlength: [6, "The length of password can be minimum 6 characters"],
+      set: (v) => bcrypt.hashSync(v, bcrypt.genSaltSync(10)),
+    },
+    image: {
+      type: String,
+      default: defaultImagePath,
+    },
+    address: {
+      type: String,
+      required: [true, "User address is required"],
+    },
+    phone: {
+      type: Number,
+      required: [true, "User phone number is required"],
+    },
+    isAdmin: {
+      type: Boolen,
+      default: false,
+    },
+    isBanned: {
+      type: Boolen,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("Users", userSchema);
+module.exports = User;
--- src/secret.js
+++ src/secret.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 
 const serverPort = process.env.SERVER_PORT || 3002;
 const mongodbUri = process.env.MONGODB_URI;
+const defaultImagePath =
+  process.env.DEFAULT_USER_IMAGE_PATH || "public/images/users/default.jpg";
 
-module.exports = { serverPort, mongodbUri };
+module.exports = { serverPort, mongodbUri, defaultImagePath };
