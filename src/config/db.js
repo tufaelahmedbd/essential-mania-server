f9fa171e1a3c59b6782cb90e5d5b2459187b1a77
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-08-09 00:16:08 +0600

    bug fixed

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 const { mongodbUri } = require("../secret");
 
-const connetDB = async () => {
+const connectDatabase = async (options = {}) => {
   try {
-    await mongoose.connect(mongodbUri);
+    await mongoose.connect(mongodbUri, options);
+    console.log("Connection to DB is successful");
+
+    mongoose.connection.on("error", (error) => {
+      console.error("DB connection error");
+    });
   } catch (error) {
-    console.error(`couldn't connect`);
+    console.error("Could not connected", error.toString());
   }
 };
+
+module.exports = connectDatabase;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
+const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
-app.listen(serverPort, () => {
+app.listen(serverPort, async () => {
   console.log(`server running on  at ${serverPort}`);
+
+  await connectDatabase();
 });
